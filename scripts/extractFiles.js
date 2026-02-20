53b18b6144f49e497e29b9043e620ee7b16fe902
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2026-02-21 01:03:04 +0800

    fix: use npm ci & remove backwards compatible logic

--- scripts/extractFiles.js
+++ scripts/extractFiles.js
@@ -20,24 +20,6 @@ const EXTRACT_FILE_PATTERN =
 
 const TARGET_DIR = path.join(__dirname, "..");
 const MODS_DIR = path.join(TARGET_DIR, "mods");
-const MODS_DIR_BACKUP = path.join(TARGET_DIR, "mods_backup");
-
-// Save a list of current files in the mods directory for backwards compatibility on linux
-const PREVIOUS_MODS_FILE_LIST = glob.sync(MODS_DIR + "/**/*", { nodir: true });
-
-/**
- * Get the new file name for the extracted file. If the file already exists in the mods directory (case-insensitive), keep the same name and case for backwards compatibility.
- * @param {string} fileName - The original file name from the storage.
- * @returns {string} - The new file name to be used for extraction.
- */
-const getNewFileName = (fileName) => {
-  const matchedPreviousFile = PREVIOUS_MODS_FILE_LIST.find(
-    (p) =>
-      p.toLowerCase().replace(/\\/g, "/") ===
-      fileName.toLowerCase().replace(/\\/g, "/"),
-  );
-  return (matchedPreviousFile || fileName).replace(/\\/g, "/");
-};
 
 // Initialize Storage
 const storage = new Storage();
@@ -45,11 +27,10 @@ const storage = new Storage();
 // Open the storage for hero and us region
 storage.openOnline(`${TEMP_DIR}*${PRODUCT}*${REGION}`);
 
-// Rename current mods directory to mods_old for backup
+// Delete existing mods directory if exists
 if (fs.existsSync(MODS_DIR)) {
-  fs.renameSync(MODS_DIR, MODS_DIR_BACKUP);
-  if (fs.existsSync(MODS_DIR_BACKUP)) fs.rmSync(MODS_DIR_BACKUP, { recursive: true, force: true });
-  console.log(`Renamed existing mods directory to ${MODS_DIR_BACKUP} for backup.`);
+  fs.rmSync(MODS_DIR, { recursive: true, force: true });
+  console.log(`Deleted existing mods directory.`);
 }
 
 // Get full list of game file first for better performance
@@ -76,20 +57,19 @@ console.log(
 filteredFiles.forEach((file, i) => {
   // Replace the "\" with "/" in the file name to ensure the correct path structure
   file.fileName = file.fileName.replace(/\\/g, "/");
-  const newFileName = getNewFileName(file.fileName);
   console.log(
-    `[${i + 1}/${filteredFiles.length}] Extracting file: fileSize=${file.contentFlags}\ts=${file.fileName === newFileName}\tfileName=${newFileName}`,
+    `[${i + 1}/${filteredFiles.length}] Extracting file: fileSize=${file.contentFlags}\tfileName=${file.fileName}`,
   );
-  const openedFile = storage.openFile(newFileName);
+  const openedFile = storage.openFile(file.fileName);
   if (openedFile) {
     const openedFileContent = openedFile.readAll();
-    const targetFilePath = path.join(TARGET_DIR, newFileName);
+    const targetFilePath = path.join(TARGET_DIR, file.fileName);
     // ensure the target directory exists
     fs.mkdirSync(path.dirname(targetFilePath), { recursive: true });
     // write the file content to the target file path
     fs.writeFileSync(targetFilePath, openedFileContent, { encoding: "utf8" });
   } else {
-    console.error(`Failed to open file: ${newFileName}`);
+    console.error(`Failed to open file: ${file.fileName}`);
   }
   // close the file handle
   openedFile.close();
@@ -97,5 +77,3 @@ filteredFiles.forEach((file, i) => {
 
 // Close the storage
 storage.close();
-
-fs.rmSync(MODS_DIR_BACKUP, { recursive: true, force: true });
